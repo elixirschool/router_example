9964ff706e0cb345c4fa73cf3bc4ddbc34a4b9a9
Sean Callan <seancallan@gmail.com>
2019-01-24 15:47:51 -0700

    Incorporate feedback

--- lib/router_example/stubs.ex
+++ lib/router_example/stubs.ex
@@ -1,9 +1,9 @@
 defmodule RouterExample.Stubs do
   def portfolio_entries do
-    for x <- 1..10, do: %{name: "Project #{x}", image: "https://picsum.photos/400/300/?random"}
+    for x <- 1..10, do: %{name: "Project #{x}", image: "https://picsum.photos/400/300/?random&t=#{x}"}
   end
 
   def submit_contact(params) do
-    IO.inspect(params)
+    IO.inspect(params, label: "Submitted Contact")
   end
 end
